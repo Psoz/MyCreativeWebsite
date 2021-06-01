a14df6ff6e95206cc6fb9eb50cdc8433d0b0848c
Paulo Sousa <paulo.sousa@farfetch.com>
2021-06-01 18:23:27 +0100

    feat: Add detail

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
+require('dotenv').config()
+
 const express = require('express')
+const errorHandler = require('errorhandler')
 const path = require('path')
 const app = express()
 const port = 3000
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req: req
+  })
+}
+
+const handlelinkResolver = doc => {
+  return '/'
+}
+
+app.use(errorHandler())
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handlelinkResolver
+  }
+  // add PrismicDOM in locals to access them in templates.
+  res.locals.PrismicDOM = PrismicDOM
+  next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -10,16 +39,34 @@ app.get('/', (req, res) => {
   res.render('pages/home')
 })
 
-app.get('/about', (req, res) => {
-  res.render('pages/about')
+app.get('/about', async (req, res) => {
+  const api = await initApi(req)
+  const about = await api.getSingle('about')
+  const meta = await api.getSingle('meta')
+
+  res.render('pages/about', {
+    about,
+    meta
+  })
 })
 
 app.get('/collections', (req, res) => {
   res.render('pages/collections')
 })
 
-app.get('/detail/:id', (req, res) => {
-  res.render('pages/detail')
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req)
+  const meta = await api.getSingle('meta')
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title'
+  })
+
+  console.log('product',product.data.highlights)
+
+  res.render('pages/detail', {
+    product,
+    meta
+  })
 })
 
 app.listen(port, () => {

8fd65d4d0c508bd198d47edfafa8ac6bfc445366
Paulo Sousa <paulo.sousa@farfetch.com>
2021-06-17 23:58:48 +0100

    feat: Finish responsive

--- app/index.js
+++ app/index.js
@@ -1,1 +1,21 @@
-console.log('Hey');
+import About from 'pages/About'
+import Collections from 'pages/About'
+import Detail from 'pages/About'
+import Home from 'pages/About'
+
+class App {
+  constructor () {
+    console.log('')
+  }
+
+  createPages () {
+    this.pages = {
+      about: new About(),
+      collections: new Collections(),
+      detail: new Detail(),
+      home: new Home()
+    }
+  }
+}
+
+new App()
